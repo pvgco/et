2a7aae2db5e3cdcbfa5d0f4fc333c0e04da5af6a
pvgco <118873687+pvgco@users.noreply.github.com>
2022-11-26 18:38:26 +0530

    Add files via upload

---
+++ orphan.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+int main()
+{
+     pid_t p;
+     p=fork();
+     if(p==0) //child
+     {   sleep(10);
+         printf("I am child having PID %d\n",getpid());
+         printf("My parent PID is %d\n",getppid());
+     }
+     else //parent
+     {
+         printf("I am parent having PID %d\n",getpid());
+         printf("My child PID is %d\n",p);
+     }
+}
