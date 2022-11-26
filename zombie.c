f7569fd59c64f61c4bf477bc91fbaee68a9ec22c
pvgco <118873687+pvgco@users.noreply.github.com>
2022-11-26 18:49:30 +0530

    Add files via upload

---
+++ zombie.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+int main()
+{
+     pid_t p;
+     p=fork();
+     if(p==0) //child
+     {
+         printf("I am child having PID %d\n",getpid());
+         printf("My parent PID is %d\n",getppid());
+     }
+     else //parent
+     {   sleep(8);
+         printf("I am parent having PID %d\n",getpid());
+         printf("My child PID is %d\n",p);
+     }
+ }
